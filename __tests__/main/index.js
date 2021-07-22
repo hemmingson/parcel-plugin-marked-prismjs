9b9d3b131d71188fc3964571dd36569eb987257f
hemmingson <287969392@qq.com>
2021-07-22 10:31:35 +0800

    Upgrade patch version
    add ci

---
+++ __tests__/main/index.js
@@ -0,0 +1,32 @@
+const marked = require('marked')
+
+const renderer = require('../../src/renderer')
+
+const source = `
+## Subtitle With Anchor
+
+\`line highlight\`
+
+\`\`\`js{4,10}
+window.confidence = true
+
+const dev = {
+  confidence: false,
+  success() {
+    return this.confidence
+  },
+}
+
+console.log((0, dev.success)()) // true
+\`\`\`
+
+\`diff\`
+
+\`\`\`diff-js
+- delete dev.confidence
++ const { confidence, ...$dev } = dev
++ dev = $dev
+\`\`\`
+`
+
+module.exports = marked(source, { renderer })
